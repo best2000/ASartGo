51f24bda5fd54fefe444d63e4575ecfc878691ef
best2000 <52110122+best2000@users.noreply.github.com>
2019-09-18 05:34:29 +0700

    new era
    yee

--- pixgen.go
+++ pixgen.go
@@ -1,26 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"io/ioutil"
 	"os"
-	"runtime"
-	"strconv"
+	"path/filepath"
 	"time"
-	"encoding/json"
 
 	"github.com/disintegration/imaging"
 	"github.com/fatih/color"
 )
 
+type Config struct {
+	Tone []string `json:"Tone"`
+	ResizeMul float64 `json:"ResizeMul"`
+}
+
 func main() {
-	/*ani := []rune("◴◷◶◵")
-	for i := 0; i < len(ani); i++ {
-		fmt.Print("\r",string(ani[i]), " ")
-		time.Sleep(100*time.Millisecond)
-	}*/
+	//start
 	fmt.Println("Starting...")
 	fbar := []rune("[          ]")
 	fmax := 100
@@ -42,12 +43,10 @@ func main() {
 			fmt.Print("\rLOADING ", string(fbar), percent, "%")
 		}
 		fcount++
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(4 * time.Millisecond)
 	}
-	opsys := runtime.GOOS
-	fmt.Println("\nOS:", opsys)
 
-	yellsty := color.New(color.FgYellow, color.Bold)
+	yellsty := color.New(color.FgYellow)
 	yellsty.Println(` 
 	 ______   ______   ______   __   __       ______   ______  ______  
 	/\  __ \ /\  ___\ /\  ___\ /\ \ /\ \     /\  __ \ /\  == \/\__  _\ 
@@ -55,12 +54,31 @@ func main() {
 	 \ \_\ \_\\/\_____\\ \_____\\ \_\\ \_\    \ \_\ \_\\ \_\ \_\ \ \_\ 
 	  \/_/\/_/ \/_____/ \/_____/ \/_/ \/_/     \/_/\/_/ \/_/ /_/  \/_/ 
 																	   `)
-	fmt.Print("Image path: ")
-	var path string
-	fmt.Scanln(&path)
+	//pull config.json info
+	set := Config{Tone: []string{"██", "▓▓", "▒▒", "░░", "  "}, ResizeMul: 1} //default setting
+	jstr, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println("err:", err)
+		os.Exit(2)
+	} else {
+		json.Unmarshal(jstr, &set) //json string to struct
+	}
+	fmt.Println("Preset")
+	fmt.Println(" Tone:", set.Tone)
+	fmt.Println(" ResizeMul:", set.ResizeMul, "\n")
+
+	//check file in
+	var finfo []os.FileInfo
+	filepath.Walk("in", func(path string, info os.FileInfo, err error) error {
+		finfo = append(finfo, info)
+		return nil
+	})
+	tempin := finfo[1].Name()
+	fmt.Println("Img:", tempin)
+	//fmt.Println("Size(bytes):", finfo[1].Size())
 
 	//open image
-	f, err := os.Open(path)
+	f, err := os.Open("in/" + tempin)
 	if err != nil {
 		fmt.Println("err:", err)
 		os.Exit(2)
@@ -73,13 +91,9 @@ func main() {
 		fmt.Println("err:", err)
 		os.Exit(2)
 	}
-
 	//resize if needed
-	fmt.Print("Resize mul: ")
-	var sizemulstr string
-	fmt.Scanln(&sizemulstr)
 	size := im.Bounds().Size()
-	sizemul, err := strconv.ParseFloat(sizemulstr, 64)
+	sizemul := set.ResizeMul
 	if err != nil {
 		fmt.Println("err:", err)
 		os.Exit(2)
@@ -87,37 +101,47 @@ func main() {
 	resize := []int{int(float64(size.X) * sizemul), int(float64(size.Y) * sizemul)}
 	reim := imaging.Resize(im, resize[0], resize[1], imaging.Lanczos)
 	reimSize := reim.Bounds().Size()
+	switch set.ResizeMul {
+	case 1:
+		fmt.Println("Size:", im.Bounds().Size().X, "x", im.Bounds().Size().Y)
+	default:
+		fmt.Println("Size:", im.Bounds().Size().X, "x", im.Bounds().Size().Y, "=>", reimSize.X, "x", reimSize.Y,)
+	}
 
 	//setup
 	tone := []int{13106, 26213, 39319, 52425, 65534}
-	strTone := []string{"██", "▓▓", "▒▒", "░░", "__"}
-	//strTone := []string{"&&","$$","MM","OO","__"}
+	strTone := set.Tone
 	strArt := ""
-	fmt.Println("reading and converting pixel...")
+	fmt.Println("Reading & converting pixel...")
 
-	//lololo
+	//laoding bar setup
 	bar := []rune("[          ]")
 	max := reimSize.Y - 1
 	tenper := max / 10
 	prog := 0
 	count := 0
+
 	//read RGBA value pixel by pixel => convert to gray value => add to string
 	for y := 0; y < reimSize.Y; y++ {
+		//bar compo
 		if count == tenper {
 			prog++
+			bar[prog] = []rune("=")[0]
 			count = 0
 		}
-		percent := int(float64(float64(y)/float64(max)) * float64(100))
-		switch prog {
-		case 0:
-			prog = 0
-			fmt.Print("\rLOADING ", string(bar), percent, "%")
-		default:
-			bar[prog] = []rune("=")[0]
-			fmt.Print("\rLOADING ", string(bar), percent, "%")
-		}
 		count++
+		//bar compo
 		for x := 0; x < reimSize.X; x++ {
+			//bar compo
+			percent := int(float64(float64(y)/float64(max)) * float64(100))
+			fmt.Print("\rLOADING ", string(bar), percent, "% [y: ", y+1, "/", reimSize.Y, "] [x: ", x+1, "/", reimSize.X, "]")
+			/*switch y {
+			case reimSize.Y-1:
+				fmt.Print("\rLOADING ", string(bar), percent, "%                                                               ")
+			default:
+				fmt.Print("\rLOADING ", string(bar), percent, "% [y: ", y+1, "/", reimSize.Y,"] [x: ", x+1, "/", reimSize.X, "]")
+			}*/
+			//bar compo
 			r, g, b, _ := reim.At(x, y).RGBA()
 			gray := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
 			grayInt := int(gray)
@@ -137,49 +161,44 @@ func main() {
 		strArt += "\n"
 	}
 
-	//create .txt file => write string to file
-	tex, err := os.Create(f.Name() + ".txt")
+	//create text.txt file => write string to file
+	tex, err := os.Create("out/text.txt")
 	if err != nil {
 		fmt.Println("err:", err)
 		os.Exit(2)
 	}
 	tex.WriteString(strArt)
-	//create .html file => write string to file
-	html, err := os.Create(f.Name() + ".html")
+
+	//create text.html file => write string to file
+	html, err := os.Create("out/text.html")
 	if err != nil {
 		fmt.Println("err:", err)
 		os.Exit(2)
 	}
-	var style string
-	switch opsys {
-	case "windows":
-		style = `style="font-family: Courier; font-size: 10px;"`
-	case "linux":
-		style = `style="font-family: monospace; font-size: 10px;"`
-	default:
-		style = ""
-	}
+	html.WriteString(`<pre id="disp" style="font-family: Courier; font-size: 10px;"` + ">\n" + strArt + "\n</pre>\n")
 
-	html.WriteString(`<button id="conv" style="font-size: 30px">Convert to image</button><pre id="ty" style="display: inline-block">  Powered by <a href="https://html2canvas.hertzen.com/">html2canvas</a></pre>
-	<script>
-		document.getElementById("conv").addEventListener('click', (e)=>{
-		  let temp = document.createElement('pre')
-		  temp.textContent = " Loading..."
-		  let canvas = html2canvas(document.body)
-		  canvas.then((re) => {
-			let temp = document.createElement('pre')
-			document.body.replaceWith(re)
-		  })
-		  document.getElementById("conv").replaceWith(temp)
-		  document.getElementById("disp").remove()
-		  document.getElementById("ty").remove()
-		})
-	  </script>
-	  <script type="text/javascript" src="html2canvas.js"></script>
-	  <pre id="disp "` + style + ">\n" + strArt + "\n</pre>")
+	//create img.html file => write string to file
+	htmlimg, err := os.Create("out/img.html")
+	if err != nil {
+		fmt.Println("err:", err)
+		os.Exit(2)
+	}
+	htmlimg.WriteString(
+		`<pre id="disp" style="font-family: Courier; font-size: 10px;"` + ">\n" + strArt + "\n</pre>\n")
+
+	//html2canvas
+	jsbyte, _ := ioutil.ReadFile("html2canvas.min.js")
+	htmlimg.WriteString("<script>\n" + string(jsbyte) + "\n</script>\n")
+
+	htmlimg.WriteString(`
+	<script>	  
+		let canvas = html2canvas(document.body)
+  		canvas.then((re) => {
+		document.body.replaceWith(re)
+  		})
+	</script>`)
 
 	// ***recommend fonts: *Inversionz, courier
 
 	fmt.Println("\nComplete...")
-
 }
